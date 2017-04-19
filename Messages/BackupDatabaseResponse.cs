16ba53b5f56119198b2db9083737ad90b830d586
Ronald Meijboom <ronaldme@gmail.com>
2017-04-19 20:26:16 +0200

    Add solution file with Messages project

---
+++ Messages/BackupDatabaseResponse.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Messages
+{
+    public class BackupDatabaseResponse
+    {
+        public bool Success { get; set; }
+        public TimeSpan DatabaseBackupTime { get; set; }
+    }
+}
